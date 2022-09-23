8fa181576fda367320e62d25c364b9b8d470d32e
souravp-lti <sourav.padhi@lntinfotech.com>
2022-09-23 15:28:43 +0530

    new commit

--- src/test/java/HBI_UI/Test_HBI_Foreign_dashboard_visible.java
+++ src/test/java/HBI_UI/Test_HBI_Foreign_dashboard_visible.java
@@ -22,7 +22,7 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
 import utils.takeExcelData;
-
+//
 
 @Listeners(Allure_Listener.class)   //IMPORTANT TO KNOW WHEN TO GO FOR SCREENSHOT
 public class Test_HBI_Foreign_dashboard_visible extends browserSelection{
